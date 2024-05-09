e4d870c4eee5f4f1cb328bacce802a18f45b75d3
Tim Su <timsu@users.noreply.github.com>
2024-05-09 14:40:17 -0700

    Flesh out the project a bit more

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # webby
 
-To install dependencies:
+This is a bun project. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+
+1. Install bun: https://bun.sh/docs/installation
+
+   (you can also use `asdf` or `mise`)
+
+2. Install dependencies:
 
 ```bash
 bun install
 ```
 
-To run:
+3. Run it:
 
 ```bash
 bun run index.ts
 ```
 
-This project was created using `bun init` in bun v1.1.4. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+This project was created using `bun init` in bun v1.1.4. 
--- index.ts
+++ index.ts
@@ -1,15 +1,41 @@
 import * as cheerio from "cheerio";
-import fs from 'fs'
+import fs from "fs";
 
-const limit = 10
-const depth = 1
+// how many links per page to fetch
+const limitPerPage = 10
+
+// how deep to traverse from the initial page
+const depth = 2
 
 async function fetchPage(url: string) {
   console.log(`Loading site: ${url}`);
   const response = await fetch(url);
   const html = await response.text();
+  console.log('got', Math.round(html.length / 1024), 'kb of html');
+
+  processPage(url, html);
   return html
-}   
+}
+
+function processPage(url: string, html: string) {
+  const $ = cheerio.load(html);
+
+  // clean up the page
+  ['header', 'script', 'header', 'nav', '#p-lang-btn', '.infobox'].forEach((selector) => {
+    $(selector).remove()
+  })
+
+  // output html
+  if (!fs.existsSync('output')) fs.mkdirSync('output')
+  fs.writeFileSync('output/page.html', `<html><body>${$.html()}</body></html>`)
+
+  const links = $('a[href^="/wiki/"]')
+  const linkUrls = links.map((i, el) => {
+    return $(el).attr('href');
+  }).get()
+
+  console.log('links', linkUrls.slice(0, 20));
+}
 
 const url = process.argv[2];
 if (!url) {
@@ -17,10 +43,7 @@ if (!url) {
   process.exit(1);
 }
 
-const html = await fetchPage(url);
-console.log('got html', html.length);
+await fetchPage(url);
 
-const $ = cheerio.load(html);
 
-const links = $('a').length
-console.log('links', links);
+
